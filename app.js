5d30e8254fffb168647438e8149608abbbbe40f7
ritik thapar <ritikthapar123@gmail.com>
2022-03-18 11:10:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+let port=process.env.PORT || 3000
+
+var app=express();
+
+app.set('view engine','ejs')
+app.use(express.static('./public'))
+
+app.get('/main',function(req,res){
+    res.render('main')
+})
+
+app.listen(port,function(){
+    console.log('you are listen to port 3000');
+});
+ 
