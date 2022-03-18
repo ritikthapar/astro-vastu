005f242761e1994260ff89b575e313819e938ebf
ritik thapar <ritikthapar123@gmail.com>
2022-03-18 11:19:16 +0530

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app=express();
 app.set('view engine','ejs')
 app.use(express.static('./public'))
 
-app.get('/main',function(req,res){
+app.get('/',function(req,res){
     res.render('main')
 })
 
